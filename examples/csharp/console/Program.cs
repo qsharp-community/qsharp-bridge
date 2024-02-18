ff2ca0d4ceef0b69ef6c7743a1adf0300d5e05c1
filipw <wojcieszyn@gmail.com>
2024-02-18 07:25:05 +0100

    added C# example

---
+++ examples/csharp/console/Program.cs
@@ -0,0 +1,30 @@
+﻿using uniffi.qsharp_bridge;
+
+var qsharpSource = """
+namespace MyQuantumApp {
+        @EntryPoint()
+        operation Main() : Unit {
+            Message("Hello");
+        }
+}
+""";
+
+Console.WriteLine("Shots: 10");
+var resultShots = QsharpBridgeMethods.RunQsShots(qsharpSource, 10);
+for (var i=0; i<10; i++) {
+    Console.WriteLine();
+    Console.WriteLine($"Shot {i+1} of 10");
+    PrintOutcome(resultShots[i]);
+    Console.WriteLine();
+}
+
+void PrintOutcome(ExecutionState result)
+{
+    Console.WriteLine("Messages:");
+    foreach (var msg in result.messages) {
+        Console.WriteLine($"  {msg}");
+    }
+
+    Console.WriteLine("Output:");
+    Console.WriteLine($"  {result.result}");
+}
